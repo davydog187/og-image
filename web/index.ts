bbfb10f8dd255e5285dc00672335310805ebdd87
Dave Lucia <davelucianyc@gmail.com>
2022-07-31 10:18:44 -0400

    Add pineapple

--- web/index.ts
+++ web/index.ts
@@ -145,13 +145,14 @@ const markdownOptions: DropdownOption[] = [
 ];
 
 const imageLightOptions: DropdownOption[] = [
+    { text: "Pineapple", value: "https://github.com/davydog187/og-image/blob/main/public/pineapple.jpg?raw=true" },
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
-
+    { text: "Pineapple", value: "https://github.com/davydog187/og-image/blob/main/public/pineapple.jpg?raw=true" },
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
